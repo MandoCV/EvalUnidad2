4249bfeb51115181104c8bfcd9519c5fea274dc3
MandoCV <210124@utxicotepec.edu.mx>
2024-06-21 13:42:26 -0600

    organizacion api

--- backend/app.py
+++ backend/app.py
@@ -2,7 +2,9 @@ from fastapi import FastAPI
 from routes.persona import persona
 from routes.usuario import usuario
 
-app= FastAPI()
-app.include_router(persona)
-app.include_router(usuario)
+app = FastAPI()
+
+app.include_router(persona, prefix="/persona", tags=["persona"])
+app.include_router(usuario, prefix="/usuario", tags=["usuario"])
+
 print("Bienvenido a mi aplicación")
