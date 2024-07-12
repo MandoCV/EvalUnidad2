36e781424742254f729ac5c37f0b21db126a5c02
Mando CV <210124@utxicotepec.edu.mx>
2024-07-12 14:01:54 -0600

    implementacion de tablas roles y roles usuario

--- backend/models/users.py
+++ backend/models/users.py
@@ -1,17 +1,28 @@
-from sqlalchemy import Column, Integer, String, Boolean, DateTime
+from sqlalchemy import Column, Integer, String, Boolean, DateTime, ForeignKey, Enum
 from sqlalchemy.dialects.mysql import LONGTEXT
 from sqlalchemy.orm import relationship
 from config.db import Base
+import models.persons
+import enum
+
+class MyEstatus(enum.Enum):
+    Activo = "Activo"
+    Inactivo = "Inactivo"
+    Bloqueado = "Bloqueado"
+    Suspendido = "Suspendido"
+    
 
 class User(Base):
-    __tablename__ = "users"
+    __tablename__ = "tbb_usuarios"
 
-    id = Column(Integer, primary_key=True, index=True)
-    usuario = Column(String(255))
-    password = Column(LONGTEXT)
-    created_at = Column(DateTime)
-    estatus = Column(Boolean, default=False)
-    Id_persona = Column(Integer)
+    ID = Column(Integer, primary_key=True, index=True)
+    Persona_ID = Column(Integer, ForeignKey("tbb_personas.ID"))
+    Nombre_Usuario = Column(String(60))
+    Correo_Electronico = Column(String(100))
+    Contrasena = Column(String(40))
+    Numero_Telefonico_Movil = Column(String(20))
+    Estatus = Column(Enum(MyEstatus))
+    Fecha_Registro = Column(DateTime)
+    Fecha_Actualizacion = Column(DateTime)
+    
 
-    # Relación con UsuariosRoles
-    roles = relationship("UsuariosRoles", back_populates="usuario")
