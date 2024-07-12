e4f88f0af1e2da488c97cc072e96aee2c38a60bd
Mando CV <210124@utxicotepec.edu.mx>
2024-07-12 14:02:13 -0600

    implementacion tabla roles y roles usuarios

--- backend/app.py
+++ backend/app.py
@@ -1,21 +1,15 @@
 from fastapi import FastAPI
-from config.db import engine, Base
 from routes.user import user
 from routes.person import person
-from routes.role import role
-from routes.usuarios_roles import usuarios_roles
+from routes.rol import rol
+from routes.userrol import userrol
 
-app = FastAPI(
+app=FastAPI(
     title="PRIVILEGE CARE",
+    description="API PARA HOSPITAL"
 )
-
-# Crear todas las tablas en la base de datos
-Base.metadata.create_all(bind=engine)
-
-# Incluir las rutas y dependencias
 app.include_router(user)
 app.include_router(person)
-app.include_router(role)
-app.include_router(usuarios_roles)
-
-print("Hola, bienvenido a mi backend")
+app.include_router(rol)
+app.include_router(userrol)
+print ("Hola bienvenido a mi backend")
--- backend/config/db.py
+++ backend/config/db.py
@@ -1,15 +1,21 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:1234@localhost:3307/hospital"
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+
+
 
 # URI para la base de datos MySQL
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://avnadmin:AVNS_YPv9gKHJKOhamUwUu3N@mysql-e331420-utxicotepec-f492.e.aivencloud.com:20907/defaultdb"
+#SQLALCHEMY_DATABASE_URL = "mysql+pymysql://avnadmin:AVNS_YPv9gKHJKOhamUwUu3N@mysql-e331420-utxicotepec-f492.e.aivencloud.com:20907/defaultdb"
 
 # Crear el motor de la base de datos
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+#engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 # Crear una sesión local
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+#SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 # Crear la base declarativa
-Base = declarative_base()
+#Base = declarative_base()
